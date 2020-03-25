9d92a9e3a212f40eeed1b75d941a0e9041d801ba
Sanchezt99 <msanchezt@eafit.edu.co>
2020-03-25 12:36:54 -0500

    Botonoes user y home del header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
-import { IonHeader, IonTitle } from '@ionic/react';
+import { IonHeader, IonIcon, IonButton} from '@ionic/react';
+import { home, person} from "ionicons/icons";
+
+/* Style */
+import "./Header.css"
 
 const Header: React.FC = () => {
     return (
         <IonHeader>
-            <IonTitle>uCommerce</IonTitle>
-        </IonHeader>
 
+            <IonButton className="home" color="tertiary" href="/Home">
+                <IonIcon icon={home}/>
+            </IonButton>
+
+            <a href="/Home">uCommerce</a>
+
+            <IonButton className="user" color="tertiary" href="/User">
+                <IonIcon icon={person}/>
+            </IonButton>
+
+        </IonHeader>
     )
 }
 
